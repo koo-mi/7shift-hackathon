808874f3d67a7461916f44a9d132ec09858c2ede
Mikael <s.h.koo1213@gmail.com>
2023-08-24 17:31:50 -0400

    initialization

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <nav className='nav'>
+
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import "./HomePage.scss";
+
+import { Button } from "@7shifts/sous-chef";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button theme="default" type="button">
+        Button
+      </Button>
+
+      
+    </div>
+  );
+};
+
+export default HomePage;
